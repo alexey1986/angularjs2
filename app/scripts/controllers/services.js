0173ca29ed9f8ec50c6671cea0b07f687682821a
unknown <Aliaksei_Paleshchuk@EPBYMINW0154.minsk.epam.com>
2014-09-05 12:16:41 +0300

    servises angular

--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -6,19 +6,11 @@ myApp.factory('requestSession', function($http){
         deleteRequest: function(dataId) {
             return $http.delete('http://54.72.3.96:3000/posts/' + dataId);
         },
-        update: function(data) {
-            return $http.put('http://54.72.3.96:3000/posts/', data);
+        addRequest: function(data) {
+            return $http.post('http://54.72.3.96:3000/posts/', data);
+        },
+        updateRequest: function(id, data) {
+            return $http.put('http://54.72.3.96:3000/posts/' + id, data);
         }
     };
 });
-
-
-/*
-myApp.factory('Posts', function ($resource) {
-    return $resource('http://54.72.3.96:3000/posts/:id', {}, {
-        update: {
-            method: 'PUT'
-        }
-    });
-});
-*/
